4b841a172320b1b47dd70866b9d5b69f857ef2e9
AfterDarcc <newrogueelement@gmail.com>
2025-02-16 01:56:15 -0500

    modified:   camera_comp.py 	modified:   interfaceGPIO.py

--- projectMain/camera_comp.py
+++ projectMain/camera_comp.py
@@ -17,16 +17,8 @@ def startCameraCapture():
      ret, frame = cap.read(0)
      return frame
 
-def makeHSVImage(baseFrame):
-     newImage = cv.cvtColor(baseFrame,cv.COLOR_RGB2HSV)
-     return newImage
-
-def makeGrayImage(baseFrame):
-     newImage = cv.cvtColor(baseFrame,cv.COLOR_RGB2GRAY)
-     return newImage
-
 def createHSVMasks(baseFrame,frameColorName):
-     loCase = string.lower(frameColorName)
+     loCase = frameColorName.lower()
      newImage = cv.inRange(baseFrame, hsvColors[loCase + "Lo"], hsvColors[loCase + "Hi"])
      return newImage
 
@@ -38,27 +30,6 @@ def apply_OR_Mask(baseFrame,maskFrame):
     newImage = cv.bitwise_or(baseFrame, baseFrame, mask= maskFrame)
     return newImage
 
-def findHSVCircles(baseFrame,returnOnlyMask):
-
-    # maskOrOverlay (0 = returns mask image only, 1 = returns mask image and ring image)
-    grayScaleFrame = makeGrayImage(baseFrame)
-    BGRFrame = cv.cvtColor(grayScaleFrame,cv.COLOR_GRAY2BGR) #BGR
-    blurBGRFrame = cv.medianBlur(BGRFrame,6) #BGR
-    circlesMask = cv.HoughCircles(blurBGRFrame,cv.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0) #BGR
-
-    if (returnOnlyMask):
-        circlesOnly = np.unint16(np.around(circlesMask)) #BGR
-        for i in circlesOnly[0, :]:
-            cv.circle(BGRFrame,(i[0],i[1]),i[2],(255,0,0),2) #draws a red circle around the circle
-            cv.circle(BGRFrame,(i[0],i[1]),2,(0,0,255),3) #draws a blue dot in the center of the circle
-    else:
-        circlesOnly = None
-    
-    circlesMask = cv.cvtColor(circlesMask,cv.COLOR_BGR2HSV) #HSV
-    circlesRing = cv.cvtColor(circlesOnly,cv.COLOR_BGR2HSV) #HSV
-
-    return circlesMask, circlesRing
-
 def drawBoxesForColor(baseFrame,maskFrame,boxText):
     cntFrame, _ = cv.findContours(maskFrame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     for cnt in cntFrame:
@@ -67,27 +38,33 @@ def drawBoxesForColor(baseFrame,maskFrame,boxText):
         cv.putText(baseFrame, boxText, (x, y-10), cv.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
     return cntFrame
 
-def extractColoredLEDs(baseRAWframe,colors,includeRings):
-    #'colors' takes in a list of colored LEDs to be found and dispOp will display
-    _layerBW = makeGrayImage(baseRAWframe) #first makes a black and white frame to use in the circles finder
-    _layerCirclesMask,_layerCirclesRings = findHSVCircles(_layerBW, 1) #finds circles in frame
-    for string in colors:
-         
-         _layerHSVMask = createHSVMasks(baseRAWframe,string) #apply color mask for each color listed in "colors"
-         _layerDetectedLEDMask = apply_AND_Mask(_layerHSVMask,_layerCirclesMask) #apply bit mask down to each of the detected circles
-         _finalOutput = apply_AND_Mask(baseRAWframe,_layerDetectedLEDMask)
-
-         if (includeRings and _layerCirclesRings is not None):
-            _finalOutput = apply_OR_Mask(_finalOutput,_layerCirclesRings)
-        
-    return _finalOutput      
      
 while True:
-        frameRAW = startCameraCapture()
-        greenLEDs = extractColoredLEDs(frameRAW,"green",1)
+        frameRAW = startCameraCapture() #frameRaw is BGR
+        hsvFrame = cv.cvtColor(frameRAW,cv.COLOR_BGR2HSV) #hsvFrame is HSV
+        grayScaleFrame = cv.cvtColor(frameRAW,cv.COLOR_BGR2GRAY) #grayScaleFrame is Gray
+
+        colorHSVMask = createHSVMasks(hsvFrame,"GREEN") #Green HSV mask
+        blurryFrame = cv.medianBlur(grayScaleFrame,5) # only takes 3 and 5 as kernel size when using uint8
+        circlesMask = cv.HoughCircles(blurryFrame,cv.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0) #ciclesMask is gray
+
+        if circlesMask is not None:
+            circlesRings = np.uint8(np.around(circlesMask)) #circles rings is gray
+            for i in circlesRings[0, :]:
+                cv.circle(frameRAW,(i[0],i[1]),i[2],(255,0,0),2) #draws a red circle around the circle
+                cv.circle(frameRAW,(i[0],i[1]),2,(0,0,255),3) #draws a blue dot in the center of the circle
+
+            circlesRings = cv.cvtColor(circlesRings,cv.COLOR_RGB2HSV) #HSV
+        
+        circlesMask = cv.cvtColor(circlesMask,cv.COLOR_GRAY2BGR) #BGR
+        colorHSVMask = cv.cvtColor(colorHSVMask,cv.COLOR_HSV2BGR) #BGRcirclesMask
+
+        detectedLEDMask = apply_AND_Mask(colorHSVMask,circlesMask) #apply bit mask down to each of the detected circles
+        finalOutput = apply_AND_Mask(frameRAW,detectedLEDMask)
+        finalOutput = apply_OR_Mask(finalOutput,circlesRings)
 
         cv.imshow("Camera Feed", frameRAW)
-        cv.imshow("Camera with Identified LEDs", greenLEDs)
+        cv.imshow("Camera with Identified LEDs", finalOutput)
 
         key = cv.waitKey(1)
         if key == 27:
--- projectMain/interfaceGPIO.py
+++ projectMain/interfaceGPIO.py
@@ -19,35 +19,35 @@ class gpio:
 
         self._pinAssignment = {
             #---------------------MOTORS---------------------
-            "frontLeftForward": (0,1,0),  # controller 1, MotorA, IN1
-            "frontLeftBackward": (1,1,0),  # controller 1, MotorA, IN2
-            "frontRightForward": (2,1,0),  # controller 1, MotorB, IN3
-            "frontRightBackward": (3,1,0),  # controller 1, MotorB, IN4
-            "frontLeftPWM": (4,1,1),  # controller 1, MotorB, ENA
-            "frontRightPWM": (5,1,1),  # controller 1, MotorB, ENB
-            "backLeftForward": (6,1,0),  # controller 2, MotorA, IN1
-            "backLeftBackward": (7,1,0),  # controller 2, MotorA, IN2
-            "backRightForward": (8,1,0),  # controller 2, MotorB, IN3
-            "backRightBackward": (9,1,0),  # controller 2, MotorB, IN4
-            "backLeftPWM": (10,1,1),  # controller 2, MotorB, ENA
-            "backRightPWM": (11,1,1),  # controller 2, MotorB, ENB
-            "cameraGimbalServo": (12,1,0),  # independent servo (SET UP PIN TO USE SOFTWARE PWM, ALL HARDWARE USED)
+            "frontLeftForward": (11,1,0),  # controller 1, MotorA, IN1_0
+            "frontLeftBackward": (7,1,0),  # controller 1, MotorA, IN2_0
+            "frontRightForward": (5,1,0),  # controller 1, MotorB, IN3_0
+            "frontRightBackward": (6,1,0),  # controller 1, MotorB, IN4_0
+            "frontLeftPWM": (11,1,1),  # controller 1, MotorB, ENA_0
+            "frontRightPWM": (12,1,1),  # controller 1, MotorB, ENB_0
+            "backLeftForward": (19,1,0),  # controller 2, MotorA, IN1_1
+            "backLeftBackward": (16,1,0),  # controller 2, MotorA, IN2_1
+            "backRightForward": (26,1,0),  # controller 2, MotorB, IN3_1
+            "backRightBackward": (20,1,0),  # controller 2, MotorB, IN4_1
+            "backLeftPWM": (13,1,1),  # controller 2, MotorB, ENA_1
+            "backRightPWM": (21,1,1),  # controller 2, MotorB, ENB_1
+            "cameraGimbalServo": (,1,0),  # independent servo (SET UP PIN TO USE SOFTWARE PWM, ALL HARDWARE USED)
             #---------------------LEDS-----------------------
-            "frontRGB_Red": (13,1,0), # 
-            "frontRGB_Green": (14,1,0), #
+            "frontRGB_Red": (4,1,0), # 
+            "frontRGB_Green": (17,1,0), #
             #"rearRedLED": (X,X), #unused, will be directly plugged into the 3.3 volt pinb
             #---------------------BUTTONS--------------------
-            "frontBumper": (15,1,0),
+            "frontBumper": (,1,0),
             #------------------COLOR_SENSOR------------------
-            "frontRGB_send": (16,1,0), 
-            "frontRGB_recieve": (17,0),
-            "leftRGB_send": (18,1,0),
-            "leftRGB_recieve": (19,0,0),
-            "rightRGB_send": (20,1,0),
-            "rightRGB_recieve": (21,0,0),
+            "frontRGB_SDA": (18,1,0), #please review, this needs to be an I2C connection because it doesn't strictly contain data pins
+            "frontRGB_SCL": (0,0), 
+            "leftRGB_SDA": (,1,0),
+            "leftRGB_SCL": (,0,0),
+            "rightRGB_SDA": (,1,0),
+            "rightRGB_SCL": (,0,0),
             #------------------ULTRASONIC--------------------
-            "ultraTrig": (22,1,0), #ultrasonic trigger pin
-            "ultraEcho": (23,0,0) #ultrasonic feedback pin
+            "ultraTrig": (2,1,0), #ultrasonic trigger pin
+            "ultraEcho": (3,0,0) #ultrasonic feedback pin
         }
 
         for i in self.pinAssignment:
