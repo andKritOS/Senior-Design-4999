0c545f5cf3e1c1306abfa9bd3f8530962ebf58b2
AfterDarcc <newrogueelement@gmail.com>
2025-02-17 01:18:14 -0500

    modified:   interfaceGPIO.py

--- projectMain/interfaceGPIO.py
+++ projectMain/interfaceGPIO.py
@@ -1,9 +1,7 @@
-import pigpio
+import gpiozero as gpio
+import time
 
-#instances class that sets up GPIO communication to local pi
-mainPi = pigpio.pi()
-
-class gpio:
+class gpioInit:
     def __init__(self):
 
         #class variables
@@ -17,9 +15,9 @@ class gpio:
         # ONCE CONNECTED, PINS NEED TO BE CHANGED IN SOFTWARE
         # FORMAT: (PIN NUM(#), IN(0)/OUT(1), PWM (1)/NOT PWM(0))
 
-        self._pinAssignment = {
+        self.pinAsgn = {
             #---------------------MOTORS---------------------
-            "frontLeftForward": (11,1,0),  # controller 1, MotorA, IN1_0
+            "frontLeftForward": (8,1,0),  # controller 1, MotorA, IN1_0
             "frontLeftBackward": (7,1,0),  # controller 1, MotorA, IN2_0
             "frontRightForward": (5,1,0),  # controller 1, MotorB, IN3_0
             "frontRightBackward": (6,1,0),  # controller 1, MotorB, IN4_0
@@ -31,36 +29,109 @@ class gpio:
             "backRightBackward": (20,1,0),  # controller 2, MotorB, IN4_1
             "backLeftPWM": (13,1,1),  # controller 2, MotorB, ENA_1
             "backRightPWM": (21,1,1),  # controller 2, MotorB, ENB_1
-            "cameraGimbalServo": (,1,0),  # independent servo (SET UP PIN TO USE SOFTWARE PWM, ALL HARDWARE USED)
+            "cameraGimbalServo": (9,1,0),  # independent servo (SET UP PIN TO USE SOFTWARE PWM, ALL HARDWARE USED)
             #---------------------LEDS-----------------------
             "frontRGB_Red": (4,1,0), # 
             "frontRGB_Green": (17,1,0), #
             #"rearRedLED": (X,X), #unused, will be directly plugged into the 3.3 volt pinb
             #---------------------BUTTONS--------------------
-            "frontBumper": (,1,0),
+            "frontBumper": (25,1,0),
             #------------------COLOR_SENSOR------------------
             "frontRGB_SDA": (18,1,0), #please review, this needs to be an I2C connection because it doesn't strictly contain data pins
-            "frontRGB_SCL": (0,0), 
-            "leftRGB_SDA": (,1,0),
-            "leftRGB_SCL": (,0,0),
-            "rightRGB_SDA": (,1,0),
-            "rightRGB_SCL": (,0,0),
+            "frontRGB_SCL": (23,0,0), 
+            "leftRGB_SDA": (27,1,0),
+            "leftRGB_SCL": (10,0,0),
+            "rightRGB_SDA": (22,1,0),
+            "rightRGB_SCL": (24,0,0),
             #------------------ULTRASONIC--------------------
             "ultraTrig": (2,1,0), #ultrasonic trigger pin
             "ultraEcho": (3,0,0) #ultrasonic feedback pin
         }
 
-        for i in self.pinAssignment:
-            #sets pin direction
-            if (i[1] == 1):
-                mainPi.set_mode(i[0],pigpio.OUTPUT)
-            else:
-                mainPi.set_mode(i[0],pigpio.INPUT)
-            #sets PWM on or off
-            if (i[2] == 1):
-                mainPi.hardware_PWM(i[0],)
+        # pin assignments
+        #FRONT LEFT
+        motorFL = gpio.Motor(
+            self.pinAsgn["frontLeftForward"[0]], #forward
+            self.pinAsgn["frontLeftBackward"[0]], #backward
+            self.pinAsgn["frontLeftPWM"[0]], #backward
+            True,
+            None
+            )
 
-    def setPWM
+        #FRONT RIGHT
+        motorFR = gpio.Motor(
+            self.pinAsgn["frontRightForward"[0]], #forward
+            self.pinAsgn["frontRightBackward"[0]], #backward
+            self.pinAsgn["frontRightPWM"[0]], #backward
+            True,
+            None
+            )
+
+        #BACK LEFT
+        motorBL = gpio.Motor(
+            self.pinAsgn["backLeftForward"[0]], #forward
+            self.pinAsgn["backLeftBackward"[0]], #backward
+            self.pinAsgn["backLeftPWM"[0]], #backward
+            True,
+            None
+            )
+
+        #BACK RIGHT
+        motorBR = gpio.Motor(
+            self.pinAsgn["backRightForward"[0]], #forward
+            self.pinAsgn["backRightBackward"[0]], #backward
+            self.pinAsgn["backRightPWM"[0]], #backward
+            True,
+            None
+            )
+
+        #CAMERA SERVO
+        camServo = gpio.servo(
+            self.pinAsgn["cameraGimbalServo"[0]],
+            0, #initial value on startup
+            0.01, #min pw
+            1, #max pw
+            0.020, #frame width
+            None
+        )
+
+        #FRONT RED LED
+        fntRed = gpio.LED(self.pinAsgn["frontRGB_Red"[0]])
+
+        #FRONT GREEN LED
+        fntRed = gpio.LED(self.pinAsgn["frontRGB_Green"[0]])
+
+        #Bumper Switch
+        bumperSW = gpio.Button(
+            self.pinAsgn["frontBumper"[0]],
+            False, #False = pulldown resistor
+            True, #True = Active High
+            None, #no bounce time because the bumper should NEVER be touched
+            1, #time to wait until executing "when held"
+            False, #No hold repeat
+            None #Pin factory, used for SPI
+            )
+        
+        #Color sensors
+        #Left
+        clrSens_L = 
+        #Center
+        clrSens_Cnt
+        #Right
+        clrSens_R
+
+        #Ultrasonic Sensor
+        ultSon = gpio.DistanceSensor(
+            self.pinAsgn["ultraTrig"[0]],
+            self.pinAsgn["ultraEcho"[0],
+            9, #length of queue of read values
+            4.0, #max readable distance (meters)
+            0.4, #IMPORTANT! THRESHOLD DISTANCE! TRIGGER IN RANGE DISTANCE FOR SENSOR
+            False, #FALSE = report values ONLY after the queue has filled up
+            None #pin factory
+            ])
+
+        
 
     
 class moveOperations:
