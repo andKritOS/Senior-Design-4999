f97ca72bc52e2bfdc930054435c117dd73bd5ad4
AfterDarcc <newrogueelement@gmail.com>
2025-03-04 02:19:40 -0500

    new file:   multiThreadFunc.py

---
+++ projectMain/multiThreadFunc.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+def __makeThreadCamera():
+
+def __makeThreadVideoProcessing():
+
+def __makeThreadVideo
+
+def createComboThread():
+    #this functions purpose will be to create a customizable multithreaded function easily for each state as needed dynamically on the fly
+    
+
+
